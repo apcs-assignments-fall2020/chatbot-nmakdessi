9313615f8de06666ae0066bdce7cc5170ae5b7ab
nmakdessi <21nmakdessi@athenian.org>
2020-09-01 18:13:11 +0000

    Added the 5 questions and completed program

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,6 +3,29 @@ import java.util.Scanner;
 public class MyMain {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = scan.nextLine();
+        System.out.println("Hello " + name + ", it's nice to meet you!");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your favorite color, " + name + " ?");
+        String color = scan.nextLine();
+        System.out.println("Oh I like " + color + " too!");
+
+        Scanner hi = new Scanner(System.in);
+        System.out.println("What is your favorite sport, " + name + " ?");
+        String sport = scan.nextLine();
+        System.out.println("Oh wow, " + sport + " is really fun!");
+
+        Scanner bye = new Scanner(System.in);
+        System.out.println("What is your favorite food, " + name + " ?");
+        String food = scan.nextLine();
+        System.out.println(food + " is delicious :)");
+
+        Scanner inp = new Scanner(System.in);
+        System.out.println("What is your favorite subject, " + name + " ?");
+        String sub = scan.nextLine();
+        System.out.println("Awesome. I like " + sub + " too!");
 
 
         scan.close();
